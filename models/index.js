f571253d1aa6f4d9df801f1ac3ca06dc5df85689
bakaemon <69375806+bakaemon@users.noreply.github.com>
2021-05-28 16:45:12 +0700

    added require method error handlings

--- models/index.js
+++ models/index.js
@@ -15,8 +15,12 @@ const files = fs.readdirSync('models/', { withFileTypes: true })
 for (let name_module of files) {
   var name = name_module.split(".")[0];
   if (name != "index") {
-    module.exports[name] = require("./" + name_module)
-    console.log("Loaded model `" + name + "` from " + name_module);
+    try {
+      module.exports[name] = require("./" + name_module);
+      console.log("Loaded model `" + name + "` from " + name_module);
+    } catch (e) {
+      console.log("Failed to load model `" + name + "` from " + name_module);
+    }
   }
 }
 // console.log(module.exports); //for debug, comment this when not needing it
