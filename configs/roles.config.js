c89f3edb8b98a78bfd8ce70ad3fe860931f35590
bakaemon <69375806+bakaemon@users.noreply.github.com>
2021-06-13 19:50:19 +0700

    optimize role control

--- configs/roles.config.js
+++ configs/roles.config.js
@@ -2,24 +2,25 @@ const Role = require("../models/role.model");
 /**
  * Auto-generate roles after successfully connected to db
  */
-  exports.roleInitialize = async () => {
+exports.roleInitialize = async () => {
     try {
-      var roles = ["admin", "staff", "trainer", "trainee"];
-      for (var x of roles) {
-        var check_name = await Role.find({ name: x });
-        if (check_name.length == 0) await Role.create({ name: x }, (err) => {
-          if (!err) console.log("Added role '" + x + "' to database.");
-        });
-        let role_db = await Role.find({});
-        var role_names = [];
-        for (var i of role_db) role_names.push(i.name);
-        var alien_roles = role_names.filter(x => !roles.includes(x));
-        for (var j of alien_roles) {
-          await Role.deleteOne({ name: j });
-          console.log("Deleted alien role '" + j + "'");
+        //add item to this array will also add new role document to database
+        var roles = ["admin", "staff", "trainer", "trainee"]; //delete items from this array will remove the role from database
+        for (var x of roles) {
+            var check_name = await Role.find({ name: x });
+            if (check_name.length == 0) await Role.create({ name: x }, (err) => {
+                if (!err) console.log("Added role '" + x + "' to database.");
+            });
+            let role_db = await Role.find({});
+            var role_names = [];
+            for (var i of role_db) role_names.push(i.name);
+            var alien_roles = role_names.filter(x => !roles.includes(x));
+            for (var j of alien_roles) {
+                await Role.deleteOne({ name: j });
+                console.log("Deleted alien role '" + j + "'");
+            }
         }
-      }
     } catch (e) {
-      console.log(e)
+        console.log(e)
     }
-  }
+}
